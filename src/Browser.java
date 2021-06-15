9cac78afa43a9d9d2f3d371cd2d4044583b77cec
Alp Sahin <alp@sahin.club>
2021-06-15 16:19:15 +0300

    Added main method to use application

--- src/Browser.java
+++ src/Browser.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Browser extends Subject {
     private String url;
@@ -29,10 +30,27 @@ public class Browser extends Subject {
         System.out.println("Username: " + account.getUserName());
         System.out.println("Password: " + account.getPassword());
         System.out.println("Successfully signed in!");
+        needsAuthentication = false;
+        notifyObservers();
     }
 
     public String getUrl() {
         return url;
     }
 
+    public static void main(String[] args) {
+        Browser browser = new Browser();
+        LoginHandler loginHandler = new LoginHandler(browser);
+        browser.addObserver(loginHandler);
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.println("Enter url to open");
+            System.out.println("Type exit to terminate");
+            String url = scanner.nextLine();
+            if (url.equals("exit")) {
+                break;
+            }
+            browser.openUrl(url);
+        }
+    }
 }
