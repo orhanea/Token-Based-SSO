09aec2cca8c9c5e43477e575246b862adb1849e5
Alp Sahin <alp@sahin.club>
2021-06-15 16:15:55 +0300

    Fixed dummy data format

--- src/USB/FlashUSB.java
+++ src/USB/FlashUSB.java
@@ -4,8 +4,12 @@ import java.util.Map;
 
 class FlashUSB extends USBDevice {
     private static Map<String, String> encryptedData = Map.of(
-            "google.com", "asdlkasfg",
-            "facebook.com", "asdf10dgfakj"
+            "google.com",
+            "example@gmail.com 1234asdflkj\n" +
+                    "example2@gmail.com 1tg83jslfd",
+            "facebook.com",
+            "sasdf@gmail.com asdf1223\n" +
+                    "asdf20@gmail.com 5823asdf"
     );
 
     public FlashUSB() {
--- src/USB/SmartCardUSB.java
+++ src/USB/SmartCardUSB.java
@@ -4,8 +4,12 @@ import java.util.Map;
 
 public class SmartCardUSB extends USBDevice {
     private static Map<String, String> encryptedData = Map.of(
-            "google.com", "aasdfa1234",
-            "facebook.com", "asdffas2ldkg"
+            "google.com",
+            "example@gmail.com 1234asdflkj\n" +
+                    "example2@gmail.com 1tg83jslfd",
+            "facebook.com",
+            "sasdf@gmail.com asdf1223\n" +
+                    "asdf20@gmail.com 5823asdf"
     );
 
     public SmartCardUSB() {
